8b9c24e3d483663717d5e51bc051b430b31536d9
zhumazhenis <zhumazhenis@gmail.com>
2018-09-14 21:19:04 +0600

    kazakhstan

--- atom.cpp
+++ atom.cpp
@@ -1,9 +1,11 @@
-##include <iostream>
+#include <iostream>
 
 using namespace std;
 
 int main() {
   cout << "Kazakhstan" << endl;
-  
+
+  cout << "Zhumazhenis Dairabay" << endl;
+
   return 0;
 }
