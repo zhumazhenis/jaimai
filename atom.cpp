d1bdbef6cce442cfef15c18743dfe519154727f1
zhumazhenis <zhumazhenis@gmail.com>
2018-09-14 19:31:27 +0600

    added files

---
+++ atom.cpp
@@ -0,0 +1,8 @@
+##include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Kazakhstan" << endl;
+  return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Kazakhstan is the best country in the world!!!" << endl;
+	return 0;
+}
